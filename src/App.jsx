d61d1d80ab7f6cb2848c111878d5c98a59e3cc2d
PTAE43 <kritsada1696@gmail.com>
2025-08-01 18:21:08 +0700

    SetDefault: Start Tailwind!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="bg-blue-500 text-white p-4">Hello Tailwind</div>
     </>
   )
 }
 
-export default App
+export default App;
